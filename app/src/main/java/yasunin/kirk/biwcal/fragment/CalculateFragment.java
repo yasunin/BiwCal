4968b6785ad37d9543c2dd9f4c44e48945b9dea2
yasunin <yasunin09@gmail.com>
2018-02-13 16:57:21 +0700

    Give up...1

--- app/src/main/java/yasunin/kirk/biwcal/fragment/CalculateFragment.java
+++ app/src/main/java/yasunin/kirk/biwcal/fragment/CalculateFragment.java
@@ -3,11 +3,12 @@ package yasunin.kirk.biwcal.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toolbar;
+
 
 import yasunin.kirk.biwcal.MainActivity;
 import yasunin.kirk.biwcal.R;
@@ -17,17 +18,26 @@ import yasunin.kirk.biwcal.R;
  */
 
 public class CalculateFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_biwcal,container,false);
 
-        return view;
-    }
+
+    private String displayString = "";
+
+    private String tag = "23JanV1";
+    private  boolean addABoolean = false, minusAboolean = false;
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        createToolbar();
+
+
+    } // Main Method
+
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.Toolbar);
@@ -41,6 +51,24 @@ public class CalculateFragment extends Fragment {
         ((MainActivity) getActivity()).getSupportActionBar()
                 .setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).getSupportFragmentManager().popBackStack();
+            }
+        });
 
-        }
-}
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_biwcal, container, false);
+        return view;
+    }
+
+
+
+}  //Main class
+
--- app/src/main/java/yasunin/kirk/biwcal/fragment/MainFragment.java
+++ app/src/main/java/yasunin/kirk/biwcal/fragment/MainFragment.java
@@ -3,10 +3,12 @@ package yasunin.kirk.biwcal.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import yasunin.kirk.biwcal.R;
 
